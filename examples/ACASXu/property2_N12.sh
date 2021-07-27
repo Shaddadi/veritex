053c1e250f74804bc4665dd90f63f962ca4fced8
Shaddadi <sheldon.uestc@gmail.com>
2021-07-27 10:22:38 -0400

    remove old files

--- examples/ACASXu/property2_N12.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-TIMEOUT=20m
-
-timeout --foreground --signal=SIGQUIT $TIMEOUT python3 main.py --property 2 --n1 1 --n2 2 --compute_unsafety
-timeout --foreground --signal=SIGQUIT $TIMEOUT matlab -nodisplay -nodesktop -r 'run display_unsafe_inputs.m;clear;quit'
-
