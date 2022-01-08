b00e1a9ee8856535ccf9ac5f8e3813dd785035f3
Shaddadi <sheldon.uestc@gmail.com>
2022-01-08 10:59:55 -0600

    remove results

--- examples/ACASXu/verify_all_instances.sh
+++ examples/ACASXu/verify_all_instances.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python3 main_verify_p1_p4.py
-python3 main_verify_p5.py
-python3 main_verify_p6.py
-python3 main_verify_p7.py
-python3 main_verify_p8.py
-python3 main_verify_p9.py
-python3 main_verify_p10.py
+python3 verify/main_verify_p1_p4.py
+python3 verify/main_verify_p5.py
+python3 verify/main_verify_p6.py
+python3 verify/main_verify_p7.py
+python3 verify/main_verify_p8.py
+python3 verify/main_verify_p9.py
+python3 verify/main_verify_p10.py
