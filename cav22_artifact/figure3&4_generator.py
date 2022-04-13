844aaea505cbb595294a3723910427a00b48a10b
Shaddadi <sheldon.uestc@gmail.com>
2022-04-12 22:38:06 -0500

    update

--- cav22_artifact/figure3&4_generator.py
+++ cav22_artifact/figure3&4_generator.py
@@ -7,57 +7,57 @@ if not os.path.isdir('./results'):
 prop_path_list = ['../examples/ACASXu/nets/prop_1.vnnlib', '../examples/ACASXu/nets/prop_2.vnnlib']
 dims = (0,1)
 
-try:
-    network_path = '../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
-    savename = 'results/figure3(a)_original_reach_property_1_2_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(a)_original_reach_property_1_2_dims_0_1 is not plotted due to memory error')
-    pass
-
-try:
-    network_path = '../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
-    savename = 'results/figure3(b)_veritex_reach_property_1_2_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(b)_veritex_reach_property_1_2_dims_0_1 is not plotted due to memory error')
-    pass
-
-try:
-    network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
-    savename = 'results/figure3(c)_art_reach_property_1_2_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(c)_art_reach_property_1_2_dims_0_1 is not plotted due to memory error')
-    pass
-
-# Figure 3 (d)(e)(f)
-prop_path_list = ['../examples/ACASXu/nets/prop_3.vnnlib', '../examples/ACASXu/nets/prop_4.vnnlib']
-dims = (0,1)
-
-try:
-    network_path = '../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
-    savename = 'results/figure3(d)_original_reach_property_3_4_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(d)_original_reach_property_3_4_dims_0_1 is not plotted due to memory error')
-    pass
-
-try:
-    network_path = '../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
-    savename = 'results/figure3(e)_veritex_reach_property_3_4_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(e)_veritex_reach_property_3_4_dims_0_1 is not plotted due to memory error')
-    pass
-
-try:
-    network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
-    savename = 'results/figure3(f)_art_reach_property_3_4_dims_0_1'
-    run(prop_path_list, network_path, dims, savename)
-except:
-    print('figure3(f)_art_reach_property_3_4_dims_0_1 is not plotted due to memory error')
-    pass
+# try:
+#     network_path = '../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
+#     savename = 'results/figure3(a)_original_reach_property_1_2_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(a)_original_reach_property_1_2_dims_0_1 is not successfully plotted')
+#     pass
+#
+# try:
+#     network_path = '../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
+#     savename = 'results/figure3(b)_veritex_reach_property_1_2_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(b)_veritex_reach_property_1_2_dims_0_1 is not successfully plotted')
+#     pass
+#
+# try:
+#     network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
+#     savename = 'results/figure3(c)_art_reach_property_1_2_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(c)_art_reach_property_1_2_dims_0_1 is not successfully plotted')
+#     pass
+#
+# # Figure 3 (d)(e)(f)
+# prop_path_list = ['../examples/ACASXu/nets/prop_3.vnnlib', '../examples/ACASXu/nets/prop_4.vnnlib']
+# dims = (0,1)
+#
+# try:
+#     network_path = '../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
+#     savename = 'results/figure3(d)_original_reach_property_3_4_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(d)_original_reach_property_3_4_dims_0_1 is not successfully plotted')
+#     pass
+#
+# try:
+#     network_path = '../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
+#     savename = 'results/figure3(e)_veritex_reach_property_3_4_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(e)_veritex_reach_property_3_4_dims_0_1 is not successfully plotted')
+#     pass
+#
+# try:
+#     network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
+#     savename = 'results/figure3(f)_art_reach_property_3_4_dims_0_1'
+#     run(prop_path_list, network_path, dims, savename)
+# except:
+#     print('figure3(f)_art_reach_property_3_4_dims_0_1 is not successfully plotted')
+#     pass
 
 
 from examples.DRL.repair.agent_properties import *
@@ -75,7 +75,7 @@ try:
     savename = 'results/figure4(a)_original_reach_property_1_2_dims_0_1'
     run([property1, property2], network_path, dims, savename)
 except:
-    print('figure4(a)_original_reach_property_1_2_dims_0_1 is not plotted due to memory error')
+    print('figure4(a)_original_reach_property_1_2_dims_0_1 is not successfully plotted')
     pass
 
 try:
@@ -83,7 +83,7 @@ try:
     savename = 'results/figure4(b)_veritex_reach_property_1_2_dims_0_1'
     run([property1, property2], network_path, dims, savename)
 except:
-    print('figure4(b)_veritex_reach_property_1_2_dims_0_1 is not plotted due to memory error')
+    print('figure4(b)_veritex_reach_property_1_2_dims_0_1 is not successfully plotted ')
     pass
 
 dims = (0,2)
@@ -92,7 +92,7 @@ try:
     savename = 'results/figure4(c)_original_reach_property_1_2_dims_0_2'
     run([property1, property2], network_path, dims, savename)
 except:
-    print('figure4(c)_original_reach_property_1_2_dims_0_2 is not plotted due to memory error')
+    print('figure4(c)_original_reach_property_1_2_dims_0_2 is not successfully plotted')
     pass
 
 try:
@@ -100,5 +100,5 @@ try:
     savename = 'results/figure4(d)_veritex_reach_property_1_2_dims_0_2'
     run([property1, property2], network_path, dims, savename)
 except:
-    print('figure4(d)_veritex_reach_property_1_2_dims_0_2 is not plotted due to memory error')
+    print('figure4(d)_veritex_reach_property_1_2_dims_0_2 is not successfully plotted')
     pass
