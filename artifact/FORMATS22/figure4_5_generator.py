d91d70bb651bc76f48ab7ce7359516a95d7a87e4
Shaddadi <sheldon.uestc@gmail.com>
2022-06-24 22:23:29 -0400

    update figure4

--- artifact/FORMATS22/figure4_5_generator.py
+++ artifact/FORMATS22/figure4_5_generator.py
@@ -8,55 +8,55 @@ if not os.path.isdir('./results'):
 prop_path_list = ['../../examples/ACASXu/nets/prop_1.vnnlib', '../../examples/ACASXu/nets/prop_2.vnnlib']
 dims = (0,4)
 
-# try:
-#     network_path = '../../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
-#     savename = 'results/figure4(a)_original_reach_property_1_2_y_1_5'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(a)_original_reach_property_1_2_y_1_5 is not successfully plotted')
-#     pass
-#
-# try:
-#     network_path = '../../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
-#     savename = 'results/figure4(b)_veritex_reach_property_1_2_y_1_5'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(b)_veritex_reach_property_1_2_y_1_5 is not successfully plotted')
-#     pass
-#
-# try:
-#     network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
-#     savename = 'results/figure4(c)_art_reach_property_1_2_y_1_5'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(c)_art_reach_property_1_2_y_1_5 is not successfully plotted')
-#     pass
+try:
+    network_path = '../../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
+    savename = 'results/figure4(a)_original_reach_property_1_2_y_1_5'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(a)_original_reach_property_1_2_y_1_5 is not successfully plotted')
+    pass
+
+try:
+    network_path = '../../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
+    savename = 'results/figure4(b)_veritex_reach_property_1_2_y_1_5'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(b)_veritex_reach_property_1_2_y_1_5 is not successfully plotted')
+    pass
+
+try:
+    network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
+    savename = 'results/figure4(c)_art_reach_property_1_2_y_1_5'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(c)_art_reach_property_1_2_y_1_5 is not successfully plotted')
+    pass
 
 
-# dims = (0,2)
-# try:
-#     network_path = '../../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
-#     savename = 'results/figure4(a)_original_reach_property_1_2_y_1_3'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(a)_original_reach_property_1_2_y_1_3 is not successfully plotted')
-#     pass
-#
-# try:
-#     network_path = '../../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
-#     savename = 'results/figure4(b)_veritex_reach_property_1_2_y_1_3'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(b)_veritex_reach_property_1_2_y_1_3 is not successfully plotted')
-#     pass
-#
-# try:
-#     network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
-#     savename = 'results/figure4(c)_art_reach_property_1_2_y_1_3'
-#     run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
-# except:
-#     print('figure4(c)_art_reach_property_1_2_y_1_3 is not successfully plotted')
-#     pass
+dims = (0,2)
+try:
+    network_path = '../../examples/ACASXu/nets/ACASXU_run2a_2_1_batch_2000.onnx'
+    savename = 'results/figure4(a)_original_reach_property_1_2_y_1_3'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(a)_original_reach_property_1_2_y_1_3 is not successfully plotted')
+    pass
+
+try:
+    network_path = '../../examples/ACASXu/repair/logs/nnet21_lr0.001_epochs200_alpha1.0_beta0.0/repaired_model.pt'
+    savename = 'results/figure4(b)_veritex_reach_property_1_2_y_1_3'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(b)_veritex_reach_property_1_2_y_1_3 is not successfully plotted')
+    pass
+
+try:
+    network_path = 'ART/results/acas/art_test_goal_safety/repaired_network_21_safe.nnet'
+    savename = 'results/figure4(c)_art_reach_property_1_2_y_1_3'
+    run(prop_path_list, network_path, dims, savename, figsize=(2.7, 2.0))
+except:
+    print('figure4(c)_art_reach_property_1_2_y_1_3 is not successfully plotted')
+    pass
 
 
 prop_path_list = ['../../examples/ACASXu/nets/prop_3.vnnlib', '../../examples/ACASXu/nets/prop_4.vnnlib']
