66e2d67ddfcb2ddaa876f3ec7e294a424da66a07
Shaddadi <sheldon.uestc@gmail.com>
2022-06-28 16:26:16 -0400

    update sefm22

--- artifact/SEFM22/reproduce_results1.sh
+++ artifact/SEFM22/reproduce_results1.sh
@@ -13,13 +13,12 @@ cd ../../DRL/repair
 python3 repair_nnets.py
 
 # repair of acasxu using art
-cd ../../../cav22_artifact/ART/art
+cd ../../../artifact/SEFM22/ART/art
 python3 exp_acas.py
 
 # display results
 cd ../../
 python3 figure2_generator.py
 python3 figure_3_4_generator.py
-python3 table_2_3_generator.py
-
+python3 table2_generator.py
 
--- artifact/SEFM22/reproduce_results2.sh
+++ artifact/SEFM22/reproduce_results2.sh
@@ -4,7 +4,7 @@
 cd ../examples/ACASXu/verify
 python3 verify_all_instances.py
 
-# repair of acasxu
+# repair of acasxu using veritex
 cd ../repair
 python3 repair_nnets.py --all
 
@@ -12,14 +12,14 @@ python3 repair_nnets.py --all
 cd ../../DRL/repair
 python3 repair_nnets.py
 
-# ART
-cd ../../../cav22_artifact/ART/art
+# repair of acasxu using art
+cd ../../../artifact/SEFM22/ART/art
 python3 exp_acas.py
 
 # display results
-cd ../../../cav22_artifact
+cd ../../
 python3 figure2_generator.py
 python3 figure_3_4_generator.py
-python3 table_2_3_generator.py
+python3 table2_generator.py
 
 
