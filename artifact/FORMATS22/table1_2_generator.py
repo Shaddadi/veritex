b800b7cc0a461757b6ae8d9b3e8d80ff3b196692
Xiaodong Yang <32499911+Shaddadi@users.noreply.github.com>
2022-06-29 10:53:54 -0400

    Update table1_2_generator.py

--- artifact/FORMATS22/table1_2_generator.py
+++ artifact/FORMATS22/table1_2_generator.py
@@ -304,7 +304,7 @@ if __name__ == "__main__":
     else:
         sys.exit("The number of repaired instances is not correct!")
 
-    with open('results/Table2&3.txt', 'w') as f:
+    with open('results/Table1&2.txt', 'w') as f:
         f.write(tables)
     print(tables)
 
