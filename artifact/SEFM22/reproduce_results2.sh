258ce23b07b1b75e535c760de4b8030e9b72ad70
Xiaodong Yang <32499911+Shaddadi@users.noreply.github.com>
2022-11-05 10:35:36 -0400

    Update reproduce_results2.sh

--- artifact/SEFM22/reproduce_results2.sh
+++ artifact/SEFM22/reproduce_results2.sh
@@ -13,13 +13,13 @@ cd ../../DRL/repair
 python3 repair_nnets.py
 
 # repair of acasxu using art
-cd ../../../artifact/SEFM22/ART/art
+cd ../../../artifact/HSCC23/ART/art
 python3 exp_acas.py
 
 # display results
 cd ../../
 python3 figure2_generator.py
 python3 figure_3_4_generator.py
-python3 table2_generator.py
+python3 table_2_3_generator.py
 
 
