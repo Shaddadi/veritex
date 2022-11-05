7168401738e52ac23a3bf5a0801814c0a0fac987
Xiaodong Yang <32499911+Shaddadi@users.noreply.github.com>
2022-11-05 10:40:15 -0400

    Delete artifact/FORMATS22 directory

--- artifact/FORMATS22/ART/scripts/run_acas.sh
+++
@@ -1,4 +0,0 @@
-python3 art/exp_acas.py --exp_fn=test_goal_safety
-python3 art/exp_acas.py --exp_fn=test_goal_safety --no_refine
-#python3 art/exp_acas.py --exp_fn=test_goal_accuracy
-#python3 art/exp_acas.py --exp_fn=test_goal_accuracy --no_refine
--- artifact/FORMATS22/ART/scripts/run_collision.sh
+++
@@ -1,2 +0,0 @@
-python3 art/exp_collision.py
-python3 art/exp_collision.py --no_refine
